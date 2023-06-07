ace493c695b9b02d44a282284ddbb96642e59379
Jordan Dennis <jordan.dennis@gmail.com>
2023-06-07 03:24:28 +0000

    Refactored spinner function

--- spinner2.js
+++ spinner2.js
@@ -1,18 +1,21 @@
-const animFrames = ["\r|   ", "\r/   ", "\r-   ", "\r\\   "];
 
-const makeAnimLoop = function(totalTime) {
+//this fucnction creates an animated text-based spinner at the beginning of a new line/
+const makeAnimLoop = function(totalTimePerSpin, spinCount) {
+  const animFrames = ["\r|   ", "\r/   ", "\r-   ", "\r\\   "];
   // while (do frame of totalTime){
-  frameDuration = totalTime / animFrames.length;
-  // console.log("frame length: ", frameDuration);
-  for (let x = 0; x < animFrames.length; x++){
-    setTimeout(() => {
-      process.stdout.write(animFrames[x]);
-    }, x * (frameDuration + 1));
+  frameDuration = totalTimePerSpin / animFrames.length;
+  for (let loopNum = 0; loopNum < spinCount; loopNum++){
+    for (let frameNum = 0; frameNum < animFrames.length; frameNum++){
+      setTimeout(() => {
+        process.stdout.write(animFrames[frameNum]);
+      }, (totalTimePerSpin * loopNum) + (frameNum * frameDuration));
+    }
   }
   setTimeout(() => {
-    process.stdout.write("\n");
-  }, totalTime); //q1. leave a little time before closing line?
+    process.stdout.write("\r \n");
+  }, totalTimePerSpin * spinCount); //q1. leave a little time before closing line?
 };
+  
+  // console.log(animFrames)
+  makeAnimLoop(250, 10);
 
-// console.log(animFrames)
-makeAnimLoop(2000);
